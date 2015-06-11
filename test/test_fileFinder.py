be2f0adbfe06a4012e752daa97595475b66f7ad3
Szilard Nemeth <nsziszy@gmail.com>
2015-06-11 23:36:10 +0200

    [mock_fs_and_file_removal] replaced mocks for FS and files in TestFileFinder with testfixtures helper functions

--- test/test_fileFinder.py
+++ test/test_fileFinder.py
@@ -1,40 +1,62 @@
 import unittest
 import os
 
-import mock
+from testfixtures import tempdir
+
+from FileFinder import FileFinder
 
 
 __author__ = 'szyszy'
 
-from test.TestHelper import MockFsHelper
+
 class TestFileFinder(unittest.TestCase):
 
-    @mock.patch('FileFinder.os')
-    def test_cache_files_by_size(self, mock_os):
-        self.mock_fs_helper = MockFsHelper(mock_os, add_defaults=True)
-        self.mock_fs_helper.cache_in_filefinder()
+    def setUp(self):
+        self.file_finder = FileFinder()
 
-        self.assertDictEqual(self.mock_fs_helper.fs.get_last_sizes_dict(), self.mock_fs_helper.filefinder.files_by_size)
+    #TODO add more testcases to FileFinder
+    #TODO start to use generate_random_binary for file contents
+    @tempdir()
+    def test_cache_files_by_size(self, temp_dir):
+        temp_dir.write('a/b/c', b'1234')
+        temp_dir.write('a/b/d', b'12345')
+        temp_dir.write('a/b/e', b'123456')
 
-    @mock.patch('FileFinder.os')
-    def test_cache_files_by_size_differentiates_two_same_named_subfolders(self, mock_os):
-        self.mock_fs_helper = MockFsHelper(mock_os)
-        self.mock_fs_helper.add_dir('a').add_dir('b1').add_files(['a.b1.f1']).end(123456)
-        self.mock_fs_helper.add_dir('b').add_dir('b1').add_files(['a.b1.f1']).end(123456)
-        self.mock_fs_helper.cache_in_filefinder()
+        self.file_finder.cache_files_by_size([temp_dir.path])
 
-        self.assertDictEqual(self.mock_fs_helper.fs.get_last_sizes_dict(), self.mock_fs_helper.filefinder.files_by_size)
+        expected_dict = {
+            4: [os.path.join(temp_dir.path, 'a', 'b', 'c')],
+            5: [os.path.join(temp_dir.path, 'a', 'b', 'd')],
+            6: [os.path.join(temp_dir.path, 'a', 'b', 'e')],
+        }
+        self.assertDictEqual(expected_dict, self.file_finder.files_by_size)
 
-    @mock.patch('FileFinder.os')
-    def test_find_candidate_files_from_cache(self, mock_os):
-        self.mock_fs_helper = MockFsHelper(mock_os)
+    @tempdir()
+    def test_cache_files_by_size_differentiates_two_same_named_subfolders(self, temp_dir):
+        temp_dir.write('a/b1/f1', b'12345')
+        temp_dir.write('b/b1/f1', b'123456789')
 
-        self.mock_fs_helper.add_dir('a').add_dir('a1').add_files(['a.a1.f1']).end(123456)
-        self.mock_fs_helper.add_dir('b').add_dir('b1').add_files(['b.b1.f1']).end(67890)
-        self.mock_fs_helper.add_dir('c').add_dir('c1').add_files(['c.c1.f1', 'c.c1.f2']).end(333333)
-        self.mock_fs_helper.cache_in_filefinder()
+        self.file_finder.cache_files_by_size([temp_dir.path])
 
-        filefinder = self.mock_fs_helper.filefinder
-        self.assertListEqual([os.path.join('a', 'a1', 'a.a1.f1')], filefinder.find_candidate_files_matching_size_from_cache(123456))
-        self.assertListEqual([os.path.join('b', 'b1', 'b.b1.f1')], filefinder.find_candidate_files_matching_size_from_cache(67890))
-        self.assertListEqual([os.path.join('c', 'c1', 'c.c1.f1'), os.path.join('c', 'c1', 'c.c1.f2')], filefinder.find_candidate_files_matching_size_from_cache(333333))
+        expected_dict = {
+            5: [os.path.join(temp_dir.path, 'a', 'b1', 'f1')],
+            9: [os.path.join(temp_dir.path, 'b', 'b1', 'f1')],
+        }
+        self.assertDictEqual(expected_dict, self.file_finder.files_by_size)
+
+    @tempdir()
+    def test_find_candidate_files_from_cache(self, temp_dir):
+        temp_dir.write('a/a1/a.a1.f1', b'1234')
+        temp_dir.write('b/b1/b.b1.f1', b'1234567')
+        temp_dir.write('c/c1/c.c1.f1', b'123456789')
+        temp_dir.write('c/c1/c.c1.f2', b'123456789')
+
+        self.file_finder.cache_files_by_size([temp_dir.path])
+
+        self.assertListEqual([os.path.join(temp_dir.path, 'a', 'a1', 'a.a1.f1')],
+                             self.file_finder.find_candidate_files_matching_size_from_cache(4))
+        self.assertListEqual([os.path.join(temp_dir.path, 'b', 'b1', 'b.b1.f1')],
+                             self.file_finder.find_candidate_files_matching_size_from_cache(7))
+        self.assertListEqual([os.path.join(temp_dir.path, 'c', 'c1', 'c.c1.f1'),
+                              os.path.join(temp_dir.path, 'c', 'c1', 'c.c1.f2')],
+                             self.file_finder.find_candidate_files_matching_size_from_cache(9))
